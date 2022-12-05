69d048adb21fcd1b02eeb64fce34c190ff1ad5c9
Jerermy Then <jeremythen16@gmail.com>
2022-12-05 12:28:09 -0400

    Adds frontend tool. Refactors

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Checksummed addresses and EIP-1191
+# Checksummed addresses and EIP-55
+
+This is a simple non-checksummed to checksummed EVM addresses conversion tool that implements [EIP-55](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md)
+
+To learn more about checksummed addresses, check this article: 
+
+[Ethereum checksummed addresses, importance, and implementation](https://medium.com/coinmonks/ethereum-checksummed-addresses-importance-and-implementation-eef74aa3ae18)
+
+## Frontend tool
+
+To use the frontend tool, open the `index.html` file in a browser.
+
+If you list of non-checksummed addresses look like the following:
+
+```js
+0x6827b8f6cc60497d9bf5210d602C0EcaFDF7C405
+0x66b0b1d2930059407dcc30f1a2305435fc37315e
+```
+Then simply paste it in the first textarea and click on the `Convert` button. Then copy the checksummed addresses from the second, non-editable, text area.
+
+If the address belongs to a network different than Ethereum, then make sure to specify the chain id in the text box above before converting to checksummed addresses. Leave empty for Ethereum.
+
+And the result would be like:
+
+```js
+0x6827b8f6cc60497d9bf5210d602C0EcaFDF7C405
+0x66B0b1d2930059407DcC30F1A2305435fc37315E
+```
+
+To check the list of available EVM chain ids, check [chainlist.org](https://chainlist.org/)
+
+If you list of addresses look like this:
+
+```js
+[0x66b0b1d2930059407dcc30f1a2305435fc37315e,
+0x66b0b1d2930059407dcc30f1a2305435fc37315e,
+0x66b0b1d2930059407dcc30f1a2305435fc37315e]
+```
+
+Then remember to check the checkbox `Addresses in array?`. This way it returns a result like:
+
+```js
+[0x66B0b1d2930059407DcC30F1A2305435fc37315E,
+0x66B0b1d2930059407DcC30F1A2305435fc37315E,
+0x66B0b1d2930059407DcC30F1A2305435fc37315E]
+```
+
+## Backend tool
+
+To use the tool in Nodejs, simply install the `sha3` dependency with:
+
+`npm install`
+
+Then import the `checkSumAddress` function and use it like:
+
+```js
+
+import { checkSumAddress } from 'emv-checksum-address-convertor';
+
+//...
+
+const checksummedAddress = checkSumAddress(address, chainId);
+
+//...
+
+```
--- index.js
+++ index.js
@@ -1,62 +1,46 @@
-import { Keccak } from 'sha3';
+import { Keccak } from "sha3";
 
 const FROM_HEXADECIMAL = 16;
 
-const isCharacter = c => {
-    const charCode = c.charCodeAt(0);
-    return (charCode >= 97 && charCode < 123);
+const isCharacter = (c) => {
+  const charCode = c.charCodeAt(0);
+  return charCode >= 97 && charCode < 123;
 };
 
 const hashAddress = (address, chainId) => {
-    const keccak = new Keccak(256);
-    // We assume that if the chainId was sent, then it's a EIP-1991 call.
-    if(chainId) {
-        address = `${chainId}0x${address}`;
-    }
-    return keccak.update(address).digest('hex');
-}
+  const keccak = new Keccak(256);
+  // We assume that if the chainId was sent, then it's a EIP-1991 call.
+  if (chainId) {
+    address = `${chainId}0x${address}`;
+  }
+  return keccak.update(address).digest("hex");
+};
 
 /**
- * 
+ *
  * @param {string} address the Ethereum compatible address
  * @param {string} chainId the Ethereum compatible chain id (RSK, Binance Smart Chain, etc)
  * @returns {string} checkSummedAddress
  */
 export function checkSumAddress(address, chainId) {
-    address = address.toLowerCase();
-    if(address.startsWith('0x')) {
-        address = address.slice(2);
+  address = address.toLowerCase();
+  if (address.startsWith("0x")) {
+    address = address.slice(2);
+  }
+  const addressHash = hashAddress(address, chainId);
+  const addressChars = address.split("");
+  for (let i = 0; i < addressChars.length; i++) {
+    const addressChar = addressChars[i];
+    // If character at ith index is not a letter, there is no need to process it.
+    if (!isCharacter(addressChar)) {
+      continue;
     }
-    const addressHash = hashAddress(address, chainId);
-    const addressChars = address.split('');
-    for(let i = 0; i < addressChars.length; i++) {
-        const addressChar = addressChars[i];
-        // If character at ith index is not a letter, there is no need to process it.
-        if(!isCharacter(addressChar)) {
-            continue;
-        }
-        const hashChar = addressHash.charAt(i);
-        const hashCharToNumber = parseInt(hashChar, FROM_HEXADECIMAL);
-        const isHashCharValueGte8 = hashCharToNumber >= 8;
-        if(isHashCharValueGte8) {
-            addressChars[i] = addressChar.toUpperCase();
-        }
+    const hashChar = addressHash.charAt(i);
+    const hashCharToNumber = parseInt(hashChar, FROM_HEXADECIMAL);
+    const isHashCharValueGte8 = hashCharToNumber >= 8;
+    if (isHashCharValueGte8) {
+      addressChars[i] = addressChar.toUpperCase();
     }
-    return addressChars.join('');
+  }
+  return addressChars.join("");
 }
-
-const addressWithNoChecksum = '3a29282d5144cea68cb33995ce82212f4b21ccec';
-const checkSummedFromMetamask = '3A29282d5144cEa68cb33995Ce82212f4B21ccEc';
-const checkSummedFromRskBlockExplorer = '3a29282D5144cea68Cb33995cE82212F4B21CcEC';
-
-// RSK implements EIP-1991, so we send a chain id and use the 0x prefix.
-const resultForRsk = checkSumAddress(addressWithNoChecksum, 31);
-
-// Ethereum clients (Metamask, Web3js, etc) don't implement EIP-1991,
-// so we don't use a chainId nor the 0x prefix.
-const resultForMetamask = checkSumAddress(addressWithNoChecksum, '');
-
-console.log('checkSummedFromRskBlockExplorer === resultForRsk: ', checkSummedFromRskBlockExplorer === resultForRsk); // true
-console.log('checkSummedFromMetamask === resultForMetamask: ', checkSummedFromMetamask === resultForMetamask); // true
-console.log('resultForRsk === resultForMetamask: ', resultForRsk === resultForMetamask); // false, because the
-// checksum for the same address for different chains would be different.
